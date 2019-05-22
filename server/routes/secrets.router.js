edb5b41e40b0bfdf7be85fbbd160d66a6d2db1e0
Vijayalakshmi Manoharan <viji@Vijayalakshmis-MacBook-Pro.local>
2019-05-22 14:40:44 -0500

    the person can only see the allowed pages

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -3,13 +3,21 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    if(req.isAuthenticated()) {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    let queryText = `SELECT * FROM "secret" WHERE "secrecy_level"<=$1`
+    //pool.query('SELECT * FROM "secret";')
+    pool.query(queryText, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
+    }else{
+        //not logged in get out
+        res.sendStatus(403)
+      }
 });
 
 module.exports = router;
+
